313f5247d4496a6419cd9059144bd9bb7db05d8e
Alejandro Palacio <alejandrooss94@gmail.com>
2022-07-28 12:02:47 -0500

    implmentacion del menu

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 require('colors');
+const { mostrarMenu, pausa } = require('./helpers/mensajes');
 
 const main = async () => {
     console.log('Hola mundo');
+
+    let opt = '';
+
+    do {
+        opt = await mostrarMenu();
+        console.log({opt});
+        if (opt !== '0') await pausa();
+
+    } while (opt !== '0');
 }
 
 main();
